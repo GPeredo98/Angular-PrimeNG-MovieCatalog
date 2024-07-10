b48bd0df83420a5b4093130015f517eddc3266d9
Gabriel Peredo <gperedo.alvaro@gmail.com>
2024-07-10 13:06:36 -0400

    feat: using angular animations

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [SharedModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
+  animations: [
+    trigger('fadeIn', [
+      transition(':enter', [
+        style({transform: 'translateY(20%)', opacity: 0}),
+        animate('0.5s ease-in', style({transform: 'translateY(0)', opacity: 1}))
+      ])
+    ])
+  ]
 })
 export class LoginComponent {
   user: string = '';
   password: string = '';
-
-  constructor(private _router: Router) {}
+  constructor(private _router: Router) { }
 
   public login(): void {
     localStorage.setItem('loggedIn', 'true');
